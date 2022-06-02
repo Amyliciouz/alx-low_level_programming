0fffe21f7cd97fb71f6249d8f4ec12fc62918e18
Amyliciouz <amakapromise15@gmail.com>
2022-06-02 14:28:42 -0700

    just commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - return the integal of the function
+ *
+ * Description:use to print "programming is like building a mutilingual puzzle"
+ *
+ * Return: value of 0
+ *
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
