f17eedb5798eaed380082b6050e72914ada56565
Amyliciouz <amakapromise15@gmail.com>
2022-06-08 00:11:42 -0700

    commit task

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* main - dum fibonacci numbers
+* Return: return zero
+*/
+int main(void)
+{
+unsigned long count, i, j, k, sums;
+i = sums = 0;
+j = 1;
+for (count = 0; count < 50; count++)
+{
+k = i + j;
+i = j;
+j = k;
+if (k % 2 == 0 && k < 4000000)
+{
+sums += k;
+}
+}
+printf("%lu\n", sums);
+return (0);
+}
+
