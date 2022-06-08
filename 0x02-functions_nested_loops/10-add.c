09ef79db0836e839126f0612a10320d94b4115df
Amyliciouz <amakapromise15@gmail.com>
2022-06-07 19:33:16 -0700

    Commit task 9

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <math.h>
+/**
+* add - adds two numbers
+* @a: int
+* @b: int
+* Description: adds two numbers and prints sum
+* Return: returns the sum.
+*/
+int add(int a, int b)
+{
+int c = a + b;
+_putchar (c);
+return (c);
+}
