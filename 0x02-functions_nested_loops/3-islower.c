1e59b7552d2993dd62ccda1593ea7f8dd57ef43e
Amyliciouz <amakapromise15@gmail.com>
2022-06-07 17:47:46 -0700

    commit only

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+include "main.h"
+/**
+ * _islower - run code
+ * @c:int
+ * Description: checks if a character is lowercase
+ * Return: returns 1 or 0
+ */
+int _islower(int c)
+{
+if (c > 96 && c < 123)
+return (1);
+else
+return (0);
+}
