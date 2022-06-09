e8020f95ade259c6b4dbe3ce8a97a923d2adca3f
Amyliciouz <amakapromise15@gmail.com>
2022-06-09 15:36:52 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*print_line - print line based on requested number
+*@n: The character to print
+*/
+void print_line(int n)
+{
+int line;
+if (n > 0)
+{
+for (line = 0; line < n; line++)
+_putchar('_');
+}
+_putchar('\n');
+}
