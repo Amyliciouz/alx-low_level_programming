afe2843c6231ce2ce3e8d23eb315e24364485684
Amyliciouz <amakapromise15@gmail.com>
2022-06-10 01:16:22 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,43 +1,38 @@
 #include "main.h"
 /**
-* print_number - Function that prints an integer.
-* @n: int type number
-* Description: Can only use _putchar to print.
+* * print_number - print any integer using putchar
+* * @n: integer to be printed
 */
 void print_number(int n)
 {
-long m; /* power of 10 */
-int c; /* boolean check */
-long num; /* convert int to long */
-num = n;
-/* negatives*/
-if (num < 0)
-{
-num *= -1;
+int count, x, y, k;
+x = 0;
+y = 1000000000;
+if (n == 0)
+_putchar('0');
+else if (n > 0)
+n *= -1;
+else
 _putchar('-');
+for (count = 0; count < 10; count++)
+{
+if (n / y == 0 && x == 0)
+{
+y /= 10;
+continue;
 }
-/* count up */
-m = 1;
-c = 1;
-while (c)
+else if (x == 0)
 {
-if (num / (m * 10) > 0)
-m *= 10;
-else
-c = 0;
-}
-/* count down */
-while (num >= 0)
-{
-if (m == 1)
-{
-_putchar(num % 10 + '0');
-num = -1;
+_putchar(-(n / y) +'0');
+x += 1;
 }
 else
 {
-_putchar((num / m % 10) + '0');
-m /= 10;
+k = (-(n / y) % 10);
+if (k < 0)
+k *= -1;
+_putchar(k + '0');
 }
+y /= 10;
 }
 }
