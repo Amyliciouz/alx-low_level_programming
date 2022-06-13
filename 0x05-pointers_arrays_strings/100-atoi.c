169be29b04e6843bf4dc87090b4b60e9844dbdbc
Amyliciouz <amakapromise15@gmail.com>
2022-06-13 14:48:52 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,28 +1,39 @@
 #include "main.h"
 #include <stdio.h>
 /**
-* main - check the code
+*_atoi - converts a string to an integer.
+*@s: pointer to string.
 *
-* Return: Always 0.
+*Return: integer gotten.
 */
-int main(void)
+int _atoi(char *s)
 {
-int nb;
-nb = _atoi("98");
-printf("%d\n", nb);
-nb = _atoi("-402");
-printf("%d\n", nb);
-nb = _atoi("     ------++++++-----+++++--98");
-printf("%d\n", nb);
-nb = _atoi("214748364");
-printf("%d\n", nb);
-nb = _atoi("0");
-printf("%d\n", nb);
-nb = _atoi("Suite 402");
-printf("%d\n", nb);
-nb = _atoi(" +  +  -  -98 Battery Street; San Francisco, CA 94111 - USA ");
-printf("%d\n", nb);
-nb = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
-printf("%d\n", nb);
-return (0);
+int index, ind2;
+unsigned int res;
+int sign = 1;
+char now;
+index = 0;
+res = 0;
+while (*(s + index) != '\0')
+{
+now = *(s + index);
+if (now == '-')
+{
+sign *= -1;
+}
+if (now >= '0' && now <= '9')
+{
+ind2 = index;
+while (*(s + ind2) > 47 && *(s + ind2) < 58)
+{
+res = (res * 10) + *(s + ind2) -'0';
+ind2++;
+}
+break;
+}
+index++;
+}
+if (sign < 0)
+res *= sign;
+return (res);
 }
