21b028533e25ccad7da114e8338ca342e8fc5fe5
Amyliciouz <amakapromise15@gmail.com>
2022-06-16 08:20:30 -0700

    commit tje task

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -10,9 +10,9 @@ p = &n;
 * write your line of code here...
 * Remember:
 * - you are not allowed to use a
-*   - you are not allowed to modify p
-*   - only one statement
-*   - you are not allowed to code anything else than this line of code
+* - you are not allowed to modify
+* - only one statement
+* - you are not allowed to code anything else than this line of code
 */
 *(p + 5) = 98;
 /* ...so that this prints 98\n */
