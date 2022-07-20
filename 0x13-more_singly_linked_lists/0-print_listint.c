864ffdf30fd88e961d88ef63994c88890205b71b
Amyliciouz <amakapromise15@gmail.com>
2022-07-19 21:53:08 -0700

    Commit task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -13,9 +13,9 @@ size_t print_listint(const listint_t *h)
 
 	while (h != NULL)
 	{
-	printf("%d\n", h->n);
-	h = h->next;
-	count++;
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
 	}
 	return (count);
 }
